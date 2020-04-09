c0589301e67947183b04694522cd65f62d547215
Gamali-Star1 <gamalio27@gmail.com>
2020-04-09 15:35:57 +0300

    MySweetProgram for github

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MySweetProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Give us a size");
+        int size = in.nextInt();
+        int[] grades = new int[size];
+        System.out.println("Enter " + size + " number.Press enter after each");
+        for(int i = 0; i < size; i++){
+            grades[i] = in.nextInt();
+        }
+        in.close();
+        for(int i = 0; i < size; i++){
+            System.out.println(grades[i]);
+        }
     }
 }
