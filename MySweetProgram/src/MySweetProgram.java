e3380ac1f38d088755784597c03d50cba04fecad
Gamali-Star1 <gamalio27@gmail.com>
2020-04-16 10:23:23 +0300

    MySweetProgram

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -2,17 +2,8 @@ import java.util.Scanner;
 
 public class MySweetProgram {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Give us a size");
-        int size = in.nextInt();
-        int[] grades = new int[size];
-        System.out.println("Enter " + size + " number.Press enter after each");
-        for(int i = 0; i < size; i++){
-            grades[i] = in.nextInt();
-        }
-        in.close();
-        for(int i = 0; i < size; i++){
-            System.out.println(grades[i]);
-        }
+        String s1 = "Hello";
+        String s2 = new String(s1);
+        System.out.println((s1 == s2) + " " + s1.equals(s2));
     }
 }
