103a25a8136cbafb0bcc2a7b79f90ca0967b32c1
Gamali-Star1 <gamalio27@gmail.com>
2020-04-16 14:57:16 +0300

    Convert queue from file

--- MySweetProgram/src/MySweetProgram.java
+++ MySweetProgram/src/MySweetProgram.java
@@ -1,20 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MySweetProgram {
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("students.txt"));
-        List<String> students = new ArrayList<>();
-        while(sc.hasNextLine()){
-            students.add(sc.nextLine());
-        }
-        for(int i = 0; i < students.size(); i++){
-            System.out.println("name: " + students.get(i));
-        }
-        sc.close();
-
+    public static void main(String[] args){
+        LinkedList<String> names = new LinkedList<>();
+        names.push("Georgia");
+        names.push("Anna");
+        names.push("Brooklyn");
+        System.out.println(names.pop());
+        System.out.println(names.removeFirst());
+        System.out.println(names.remove());
     }
 }
