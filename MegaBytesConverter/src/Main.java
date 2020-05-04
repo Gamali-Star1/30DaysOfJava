17d2fd3111e2a0a1ba11568b378d9808068e6c20
Gamali-Star1 <gamalio27@gmail.com>
2020-05-04 17:26:36 +0300

    src/IntEqualityPrinter.java

--- MegaBytesConverter/src/Main.java
+++ MegaBytesConverter/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+        IntEqualityPrinter.printEqual(1, 2, 3);
     }
 }
