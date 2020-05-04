01581724e96fd963c73c11fe1c57b71b356f6abb
Gamali-Star1 <gamalio27@gmail.com>
2020-05-04 17:06:23 +0300

    src/MinutesToYearsDaysCalculator.java

--- MegaBytesConverter/src/Main.java
+++ MegaBytesConverter/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Area of circle = " + AreaCalculator.area(-1.0, 4.0));
-
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
     }
 }
