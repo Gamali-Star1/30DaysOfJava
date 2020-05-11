71e388a6d514534bb4d853115c4f897a8412db53
Gamali-Star1 <gamalio27@gmail.com>
2020-05-11 15:48:44 +0300

    src/Main.java

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,25 +1,34 @@
-//A program that show if first & second number has shared
-//the same digit return true otherwise return false
+//A program that compare last digit
+//A program to check if the number are within the given range
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(hasShareDigit(12, 23));
+        System.out.println(isValid(1051));
     }
-//Method to show whether first & second number has shared the same
-    //digit it will return true otherwise return false
-    public static boolean hasShareDigit(int number1, int number2){
-        //find digit in the number from the left
-        int a = number1/10;
-        int b = number2/10;
-        // find digit in the number from right
-        int c = number1 % 10;
-        int d = number2 % 10;
 
-        if(((number1 >= 10) && (number1 <= 99)) && ((number2 >= 10) && (number2 <= 90))){
-            if((a == b) || (a == c) || (a == d) || (b == c) || (b == d) || (c == d))
+    //A Method to compare a last digit if are the same within 3 numbers
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        //Find the right most digit
+        int a = num1 % 10;
+        int b = num2 % 10;
+        int c = num3 % 10;
+
+        //write if statement to check whether these numbers are within
+        //10 and 1000 inclusive and return true otherwise return false
+        if(((num1 >= 10) && (num1 <= 1000)) && ((num2 >= 10) && (num2 <= 1000)) && ((num3 >= 10) && (num3 <= 1000))){
+            if((a == b) || (a == c) || (b == c))
                 return true;
             else
                 return false;
         }return false;
+    }
+
+    //This method using one parameter of type int to if it is within
+    //10 to 1000 inclusive and return true otherwise return false
+    public static boolean isValid(int num){
+        if((num >= 10) && (num <= 1000))
+            return true;
+        else
+            return false;
     }
 }
