0717d1309808f696cab0b1d6265b08a67d5feb7d
Gamali-Star1 <gamalio27@gmail.com>
2020-05-13 11:32:08 +0300

    src/Main.java

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,28 +1,18 @@
-//A program to find a greatest common divisor
+//A program to print all factors
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getGreatestCommonDivisor(81, 153));
+        printFactors(-1);
     }
-    //A method in a program to find a greatest common divisor
-    public static int getGreatestCommonDivisor(int first, int second) {
-        int gcd = 1;
-        if ((first < 10) || (second < 10)) {
-            return -1;
-        } else {
-                if (first > second) {
-                    for (int i = second; i > 1; i--) {
-                        if ((first % i == 0) && (second % i == 0)) {
-                            return i;
-                        }
-                    }
-                } else {
-                    for (int j = first; j > 1; j--) {
-                        if ((first % j == 0) && (second % j == 0)) {
-                            return j;
-                        }
-                    }
-                }
-            }return gcd;
+    // A method that prints all factors
+    public static void printFactors(int number){
+        if(number < 1){
+            System.out.println("Invalid Value");
+        }else {
+            for (int i = 1; i <= number; i++){
+                if (number % i == 0)
+                    System.out.println(i);
+            }
+        }
     }
 }
