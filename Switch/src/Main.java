3985ff13bc6103a069907183999e52dc42fa99b3
Gamali-Star1 <gamalio27@gmail.com>
2020-05-15 13:31:24 +0300

    src/Main.java

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,21 +1,29 @@
-//A program to calculate the largest prime factor in a number
+//A program to print square star
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getLargestPrime(-1));
+        printSquareStar(8);
     }
-    //Method in program to calculate the largest prime factor in number
-    public static int getLargestPrime(int number){
-        int i;
-        if (number < 2){
-            return -1;
+
+    //A Method which prints square star after execution
+    public static void printSquareStar(int number){
+        int rowCount = number;
+        if (number < 5){
+            System.out.print("Invalid Value");
         }else {
-            for (i = 2; i <= number; i++){
-                if (number % i == 0){
-                    number = number/i;
-                    i--;
-                }
-            }return i;
+            for(int row = 1; row <= number; row++){
+                int rowNumber = row;
+                for(int column = 1; column <= number; column++){
+                    int columnNumber = column;
+                    int currentRow = rowNumber;
+                    if ((row == 1) || (row == number) || (column == 1) || (column == number) || (rowNumber == columnNumber) || (columnNumber == ((rowCount - currentRow) + 1))){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
+                }System.out.println();
+            }
+
         }
     }
 }
