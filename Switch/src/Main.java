ec95db177c9ba3be583c6ae6f758999f40ed2b20
Gamali-Star1 <gamalio27@gmail.com>
2020-05-15 11:54:50 +0300

    src/Main.java

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -1,25 +1,21 @@
-//A program to show if the number is a perfect number
+//A program to calculate the largest prime factor in a number
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPerfectNumber(-1));
+        System.out.println(getLargestPrime(-1));
     }
-
-    //A method in a program to show if the number is a perfect number
-    public static boolean isPerfectNumber(int number) {
-        int sum = 0;
-        if (number < 1) {
-            return false;
-        } else {
-                for (int j = 1; j < number; j++) {
-                    if (number % j == 0) {
-                        sum += j;
-                    }
+    //Method in program to calculate the largest prime factor in number
+    public static int getLargestPrime(int number){
+        int i;
+        if (number < 2){
+            return -1;
+        }else {
+            for (i = 2; i <= number; i++){
+                if (number % i == 0){
+                    number = number/i;
+                    i--;
                 }
-                if (sum == number)
-                    return true;
-                else
-                    return false;
-            }
+            }return i;
+        }
     }
-    }
+}
